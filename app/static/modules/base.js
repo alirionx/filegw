43b1b3b0140d2e52ef9e67d66da187b84ea44408
alirionx <dquilitzsch@outlook.de>
2020-03-18 23:58:00 +0100

    databrowser for webui

--- app/static/modules/base.js
+++ app/static/modules/base.js
@@ -1,4 +1,122 @@
 
-setTimeout(function(){
-    document.getElementById('headBlock').onclick = function(){console.log('gehder');}
-},100);
+
+addEventListener('click', function(event){
+    tgtElm = event.target;
+    if( tgtElm.getAttribute("css") != "btn" ){
+        remove_elms_by_class("actMenuFrame");
+    }
+});
+
+function folder_menu_call(event){
+    
+    remove_elms_by_class("actMenuFrame");
+    
+    event.preventDefault();
+    var tgtElm = event.target;
+    var lnk = tgtElm.getAttribute("lnk");
+
+    var snapTgt = document.getElementById("snapTarget");
+    var xPos = event.clientX + "px";
+    var yPos = event.clientY + "px";
+
+    var menuOpt = {
+        "open": lnk,
+        "download zip": "/api/zip/download"+lnk, 
+        "delete": "/folder/delete"+lnk
+    }
+    if(lnk.endsWith("../")){
+        var menuOpt = {
+            "up": lnk
+        }
+    }
+
+    var actMenuFrame = document.createElement("DIV");
+    actMenuFrame.className = "actMenuFrame";
+    snapTgt.appendChild(actMenuFrame);
+    actMenuFrame.style.top = yPos;
+    actMenuFrame.style.left = xPos;
+    
+    var actMenuArrow = document.createElement("DIV");
+    actMenuFrame.appendChild(actMenuArrow);
+    actMenuArrow.setAttribute("css", "arrow");
+    
+    for(var prop in menuOpt){
+        var actMenuBtn = document.createElement("DIV");
+        actMenuFrame.appendChild(actMenuBtn);
+        actMenuBtn.setAttribute("css", "btn");
+        actMenuBtn.setAttribute("lnk", menuOpt[prop]);
+        actMenuBtn.innerHTML = prop;
+        actMenuBtn.onclick = function(){
+            var lnk = this.getAttribute("lnk");
+            location.href = lnk;
+            var browserBox = document.getElementById("browserBox");
+            //loader_call(browserBox);
+            //console.log(lnk);
+            remove_elms_by_class("actMenuFrame");
+        }
+    }
+}
+
+function file_menu_call(event){
+    event.preventDefault();
+    var tgtElm = event.target;
+    
+}
+
+
+//-Helpers---------------------------------------------------------
+
+function remove_elms_by_class(clName){
+    var elmAry = document.getElementsByClassName(clName);
+    for (var i = 0; i < elmAry.length; i++) {
+        elmAry[i].parentNode.removeChild(elmAry[i]);
+    }
+}
+
+//--------------------------------
+
+function post_data(url){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(xhttp.responseText);
+        }
+    };
+xhttp.open("POST", url, true);
+xhttp.send();
+}
+
+//--------------------------------
+
+function loader_call(target=document.body){
+		
+    var blocker = document.createElement("div");
+    blocker.classList.add("blocker");
+
+    var loader_frame = document.createElement("div");
+    loader_frame.classList.add("spinner");
+
+    for (var i = 1; i < 4; i++) {
+
+        var loader_bounce = document.createElement("div");
+        loader_bounce.classList.add("bounce"+i);
+        loader_frame.appendChild(loader_bounce);
+    }
+
+    blocker.appendChild(loader_frame);
+    setTimeout( function(){
+        target.appendChild(blocker);
+    },200);
+}
+
+
+function loader_remove(target, sleep=0, follow_func=function(){}){
+    
+    setTimeout( function(){
+        var blocker = target.getElementsByClassName("blocker")[0];
+        target.removeChild(blocker);
+        follow_func();
+    }, sleep );
+}
+
+//---------------------------------------------------------------
--- app/webapisrv.py
+++ app/webapisrv.py
@@ -1,8 +1,16 @@
+#-To Implement Feature List-----------------------------------------------
+#--API Functions: Download, Upload, Delete, 
+#--Folder ZIP Download, ZIP Upload
+#--Action Menue in WebUI (oncontextmenue) for file and folder
+#--Folder Path bar on top
+
 #-Import needed modules---------------------------------------------------
 import os
 import sys
+import shutil
 import json
-from flask import Flask, request, redirect, url_for, send_from_directory, session, render_template
+#import zipfile
+from flask import Flask, request, redirect, url_for, send_from_directory, session, render_template, send_file
 
 #-Global and Session Vars-------------------------------------------------
 from globals import dataPath
@@ -19,30 +27,37 @@ app.debug = True
     #session['workdir'] = False
 #    session.permanent = True
 
-#-The Flask Thing (API)---------------------------------------------------
+#-HTML Render Section-----------------------------------------------------
 
 @app.route('/', methods=['GET']) 
-def html_home():
+def html_browser():
     davPath = dataPath
+    pathSplit = []
+    
+    #-Forward to root path if no path is set-
+    if request.args.get('path') == None:
+        return redirect('?path=/', code=302)
+    
+    #-handle folder up requests
+    newPath = request.args.get('path')
+    pathSplit = newPath.split('/')
+    if newPath.endswith('../'):
+        newPath = newPath.replace('../', '')
+        pathSplit = newPath.split('/')
+        if len(pathSplit) > 0:
+            pathSplit = pathSplit[:-1]
+        davPath = '/'.join(pathSplit)
 
-    if request.args.get('path') != None:
-        newPath = request.args.get('path')
-        if newPath.endswith('../'):
-            newPath = newPath.replace('../', '')
-            pathSplit = newPath.split('/')
-            if len(pathSplit) > 0:
-                pathSplit = pathSplit[:-1]
-            davPath = '/'.join(pathSplit)
-
-            return redirect('?path='+davPath, code=302)
-        else:
-            davPath += newPath
+        return redirect('?path='+davPath, code=302)
+    else:
+        davPath += newPath
 
     davPath = davPath.replace('//', '/')
 
     if not os.path.isdir(davPath):
         return 'Bad Request', 400 
 
+    #-Build the file and folder Ary-----
     dirObj = []
     dirAry = os.listdir(davPath)
     dirAry.sort()
@@ -74,18 +89,103 @@ def html_home():
             }
             dirObj.append(toAdd)
 
-    #dirJson = json.dumps(dirAry, indent=2)
-    #return '<pre>'+dirJson+'</pre>'
 
+    #-The path line navigator Ary----
+    pathObj = [{"dir":"", "lnk":"/"}]
+    pathSplit = davPath.split('/')[1:]
+    tmpSplit = []
+    for entry in pathSplit:
+        if entry != "":
+            tmpSplit.append(entry)
+            lnk = '/'.join(tmpSplit) 
+            pathObj.append( {"dir":entry, "lnk":"/"+lnk} )
+
+    #-Render HTML Template---
     return render_template(
         'browse.html', 
         view='browse',
         davPath=davPath,
-        dirAry=dirAry,
+        pathObj=pathObj,
         dirObj=dirObj
+    )
+#--------------------------------
+@app.route('/folder/delete', methods=['GET']) 
+def html_folder_delete():
+    if request.args.get('path') == None or request.args.get('path') == '/':
+        return 'parameter path not set', 400
+
+    path = request.args.get('path')
+    btPath = path.split('/')[:-1]
+    btPath = '/'.join(btPath)
+    msg = 'Wollen Sie "'+path+'" wirklich löschen?' 
+    #-Render HTML Template---
+    return render_template(
+        'confirm.html', 
+        view='folder_delete',
+        msg=msg,
+        path=path,
+        backTo='/?path='+btPath
     )
 
 
+
+#-API Section-------------------------------------------------------------
+@app.route('/api/zip/download', methods=['GET']) 
+def api_zip_download():
+    if request.args.get('path') == None:
+        return 'parameter path not set', 400
+
+    path = request.args.get('path')
+    fName = path.split('/')[-1]
+    fName = ''.join(e for e in fName if e.isalnum())
+    fPath = '/tmp/'+fName
+
+    shutil.make_archive(fPath, 'zip', path)
+
+    #zipf = zipfile.ZipFile(fName, 'w', zipfile.ZIP_DEFLATED)
+    #zipdir('tmp/', zipf)
+    #zipf.close()
+
+    #return fPath
+    #return send_file(fPath+'.zip', attachment_filename=fName+'.zip')
+    try:
+        return send_file(fPath+'.zip', as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
+
+#------------------
+
+@app.route('/api/data/delete', methods=['POST']) 
+def api_folder_delete():
+    
+    try: delPath = request.form['path'].replace('//', '/')
+    except: return 'Bad Request', 400
+
+    try: backTo =  request.form['backto'].replace('//', '/')
+    except: backTo = '/'
+
+    chk = False
+    if os.path.isfile(delPath):
+        try:
+            os.remove(delPath)
+            chk = True
+        except:
+            inf = 'unable to delete'
+    
+    if os.path.isdir(delPath):
+        try:
+            shutil.rmtree(delPath, ignore_errors=False)
+            chk = True
+        except:
+            inf = 'unable to delete' + delPath
+    
+    if chk == False: 
+        return inf, 400
+
+    print(delPath + ' = deleted')
+
+    return redirect(backTo, code=302)
+    
 
 
 #-------------------------------------------------------------------------
